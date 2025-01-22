b13b7515ffaf2baaf907e71b9a2b9e5c7ca7034d
hayanLee <hayanlee.dev@gmail.com>
2025-01-22 20:42:11 +0900

    Style : 메인 컬러 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
     return (
         <>
             <h1>메인화면</h1>
-            <h1 className='font-black'>Pretendard</h1>
+            <h1 className='font-black text-point'>Pretendard</h1>
             <h1 className='font-extrabold'>Pretendard</h1>
             <h1 className='font-bold'>Pretendard</h1>
-            <h1 className='font-semibold'>Pretendard</h1>
+            <h1 className='font-semibold text-point'>Pretendard</h1>
             <h1 className='font-medium'>Pretendard</h1>
             <h1 className='font-normal'>Pretendard</h1>
             <h1 className='font-light'>Pretendard</h1>
