699fcc7f1ef9ead1b5a3d15a9a1de6a50942f1d5
hayanLee <hayanlee.dev@gmail.com>
2025-01-22 20:49:32 +0900

    Script : shadcn 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
     return (
         <>
             <h1>메인화면</h1>
+            <Button className='bg-point'>Button</Button>
             <h1 className='font-black text-point'>Pretendard</h1>
             <h1 className='font-extrabold'>Pretendard</h1>
-            <h1 className='font-bold'>Pretendard</h1>
+            <h1 className='font-bold'></h1>
             <h1 className='font-semibold text-point'>Pretendard</h1>
             <h1 className='font-medium'>Pretendard</h1>
             <h1 className='font-normal'>Pretendard</h1>
