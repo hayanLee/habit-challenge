fe3bd71b28bf2138a03a62864a5e4e988d472a1d
hayanLee <hayanlee.dev@gmail.com>
2025-02-03 13:05:22 +0900

    Update : 완료 날짜에 스티커/미완료 날짜에 요일 보여주도록 구현

--- src/app/(root)/goals/[goalId]/page.tsx
+++ src/app/(root)/goals/[goalId]/page.tsx
@@ -1,4 +1,5 @@
 import getImageArray from '@/utils/getImageArray';
+import Image from 'next/image';
 import path from 'path';
 import StickerDrawer from './_components/StickerDrawer';
 
@@ -12,10 +13,24 @@ const GoalDetailPage = async ({ params }: GoalDetailProps) => {
 
     const data = await fetch(`http://localhost:8000/challenges/${goalId}`);
     const res = await data.json();
-    const { challengeName, startDay, endDay, period } = res;
+    const { challengeName, startDay, endDay, period, progress } = res;
+
+    const periodArr = Array.from({ length: period }, (_, idx) => {
+        const progressItem = progress[idx];
+
+        return progressItem ? (
+            <div className='day !bg-white relative border-point border-2 overflow-hidden' key={progressItem.date}>
+                <Image src={`/assets/stickers/${progressItem.sticker}`} alt='' fill className='object-contain' />
+            </div>
+        ) : (
+            <div key={idx} className='day'>
+                {idx + 1} {/* 1일부터 시작하도록 수정 */}
+            </div>
+        );
+    });
 
     // 스티커 경로
-    const directoryPath = path.join(process.cwd(), 'public/assets/stickers', `${'rabbit'}`);
+    const directoryPath = path.join(process.cwd(), 'public/assets/stickers', `${'dog'}`);
     // 모든 스티커를 담은 배열로 가져오기
     const images = getImageArray(directoryPath, 'dog');
 
@@ -29,13 +44,13 @@ const GoalDetailPage = async ({ params }: GoalDetailProps) => {
                     </p>
                 </div>
 
-                <div className='mx-10 mt-7 mb-24'>
+                <div className='mx-5 mt-7 mb-24'>
                     <div className='grid grid-cols-5 gap-3.5'>
-                        {Array.from({ length: period }, (_, idx) => idx + 1).map((day) => (
-                            <div className='day' key={day}>
-                                {day}
-                            </div>
-                        ))}
+                        {/*
+                         * 완료된 날에는 스티커
+                         * 미완료된 날에는 날짜
+                         */}
+                        {periodArr}
                     </div>
                 </div>
 
